cb3e1a0a3cd82ac54feacb529294af845a9c3a64
Shun-Sheng Li <41175095+lshuns@users.noreply.github.com>
2019-11-04 17:08:36 +0100

    add 0diagram.py to ColorSplit

--- KV450/ColorSplit/0diagram.py
+++ KV450/ColorSplit/0diagram.py
@@ -14,8 +14,8 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 
-def CMdiagramFunc(patches, inpaths, outpath, Z, bins, bandX, bandY, TB, 
-                    XLABEL, YLABEL):
+def CMdiagramFunc(patches, inpaths, outpaths, bandX, bandY, TB, 
+                    MARKSIZE, XLIM, YLIM, XLABEL, YLABEL):
     """
     Function for color-magnitude diagram
     """
@@ -24,8 +24,8 @@ def CMdiagramFunc(patches, inpaths, outpath, Z, bins, bandX, bandY, TB,
     # mpl.use('Agg')
     mpl.rcParams['xtick.direction'] = 'in'
     mpl.rcParams['ytick.direction'] = 'in'
-    #mpl.rcParams['xtick.top'] = True
-    #mpl.rcParams['ytick.right'] = True
+    mpl.rcParams['xtick.top'] = True
+    mpl.rcParams['ytick.right'] = True
     plt.rc('font', size=10)
 
 
@@ -35,31 +35,46 @@ def CMdiagramFunc(patches, inpaths, outpath, Z, bins, bandX, bandY, TB,
         patch = patches[i]
         inpath = inpaths[i]
 
-        hdf = pd.HDFStore(inpath, mode='r')
-        print("hdf built from", inpath)
+        tmp = pd.read_feather(inpath)
 
-        for i in range(len(bins)-1):
-        
-            key = Z + '__' + str(bins[i]) + str(bins[i+1])
+        data.append(tmp)
+        print("Data loaded from", inpath)
 
-            tmp = hdf.select(key=key, columns=[bandX, bandY, TB])
-            print("Selected data from", key)
-            data.append(tmp)
-        hdf.close()
-        print("Done with data selection for", patch)
     data = pd.concat(data)
 
     magx = data[bandX].values
     magy = data[bandY].values
     vTB = data[TB].values
 
-    cm = plt.cm.get_cmap('RdYlBu')
-    sc = plt.scatter(magx, magy-magx, c=vTB, vmin=0, vmax=np.amax(TB), cmap=cm)
+    # cm = plt.cm.get_cmap('RdYlBu')
+    # sc = plt.scatter(magx, magy-magx, c=vTB, s=MARKSIZE, vmin=0, vmax=np.amax(vTB), cmap=cm)
+    # plt.xlim(XLIM[0], XLIM[1])
+    # plt.ylim(YLIM[0], YLIM[1])
+    # plt.xlabel(XLABEL)
+    # plt.ylabel(YLABEL)
+    # cbar = plt.colorbar(sc)
+    # cbar.ax.set_ylabel(r'$T_B$')
+
+
+    magx_r = magx[vTB<=1.9]
+    magy_r = magy[vTB<=1.9]
+    vTB_r = vTB[vTB<=1.9]
+
+    magx_b = magx[vTB>1.9]
+    magy_b = magy[vTB>1.9]
+    vTB_b = vTB[vTB>1.9]
+
+    sc_r = plt.scatter(magx_r, magy_r-magx_r, c='red', s=MARKSIZE, label=r'$T_B\leq 1.9$')
+    sc_b = plt.scatter(magx_b, magy_b-magx_b, facecolors='none', edgecolors='blue', linewidth=0.1, s=MARKSIZE, label=r'$T_B > 1.9$')
+    plt.xlim(XLIM[0], XLIM[1])
+    plt.ylim(YLIM[0], YLIM[1])
     plt.xlabel(XLABEL)
     plt.ylabel(YLABEL)
-    plt.colorbar(sc)
+    plt.legend()
+
+    
     for outpath in outpaths:   
-        plt.savefig(outpath)
+        plt.savefig(outpath, dpi=300)
     plt.close()
 
 
@@ -75,30 +90,30 @@ if __name__ == "__main__":
     bandY = 'MAG_GAAP_g_CALIB' 
     TB = 'T_B'
 
-    bins = [1, 3, 5, 7, 9, 12]
     patches = ["G9","G12","G15","G23","GS"]
-    key = "_whole"
-    # column used as redshift
-    Z = 'Z_B'
 
     # input path
-    pathF = "/disks/shear15/ssli/KV450/CorrFunc/data/"
-    pathP = ".h5"
+    pathF = "/disks/shear15/ssli/KV450/CorrFunc/data/feather/"
+    pathP = "_allBins.feather"
     inpaths = []
     for patch in patches:
-        inpath = pathF + patch + key + pathP
+        inpath = pathF + patch + pathP
         inpaths.append(inpath)
 
     # output path 
-    outpath1 = "/disks/shear15/ssli/KV450/ColorSplit/CMdiagram.png"
-    outpath2 = "/net/raam/data1/surfdrive_ssli/Projects/6CosmicShear_RB/plot/ColorSplit/CMdiagram.png"
+    outpath1 = "/disks/shear15/ssli/KV450/ColorSplit/CMdiagram_RB.png"
+    outpath2 = "/net/raam/data1/surfdrive_ssli/Projects/6CosmicShear_RB/plot/ColorSplit/CMdiagram_RB.png"
     outpaths = [outpath1, outpath2]
 
     # plot related
+    MARKSIZE = 0.5
+    XLIM = [18, 27]
+    YLIM = [-6, 6]
     XLABEL = '$m_r$'
     YLABEL = '$m_g-m_r$'
 
-    CMdiagramFunc(patches, inpaths, outpaths, Z, bins, bandX, bandY, TB, 
-                    XLABEL, YLABEL)
+
+    CMdiagramFunc(patches, inpaths, outpaths, bandX, bandY, TB, 
+                    MARKSIZE, XLIM, YLIM, XLABEL, YLABEL)
 
 
