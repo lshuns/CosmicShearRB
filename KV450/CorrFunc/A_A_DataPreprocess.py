1c8dc5bdf2cdbebe3c1cb8994421f570014bc9f5
Shun-Sheng Li <41175095+lshuns@users.noreply.github.com>
2019-11-08 15:14:10 +0100

    build A_A_DataPreprocess.py to CorrFunc

--- KV450/CorrFunc/A_A_DataPreprocess.py
+++ KV450/CorrFunc/A_A_DataPreprocess.py
@@ -5,10 +5,21 @@ Created on Wed Oct  2 10:20:07 2019
 
 @author: ssli
 
+SelecFunc:
 Read the original data (fits)
 Set selection criteria
-Save for further use (h5)
+Save for further use (feather)
 
+BinFunc:
+build the five tomographic redshift bins:
+    "13": (0.1, 0.3]
+    "35": (0.3, 0.5]
+    "57": (0.5, 0.7]
+    "79": (0.7, 0.9]
+    "912": (0.9, 1.2]
+
+BinCountFunc:
+Function for number counting for binning results
 """
 
 # Parameters
@@ -63,30 +74,36 @@ Save for further use (h5)
 #	 'binary_cut', 'recal_weight', '2D_measurement_variance_corr']
 #
 
-
+import numpy as np
 import pandas as pd
+import feather
 from astropy.io import fits
 import multiprocessing as mp
 
 
-def SelecFunc(patch, inpath, outpath, pq):
+
+# def setup():
+#     """
+#     CallFunction
+#     """
+
+def SelecFunc(patch, inpath, outdir, pq):
     """
     Function for selection of targets
     """
-    
-    print("Input data from", inpath)
-    
-    hdf = pd.HDFStore(outpath, mode='w')
-
+    print(patch, "Start selection of targets (SelecFunc)...")
+        
     # with scope making the file be closed automatically
     with fits.open(inpath, memmap=True) as hdul:        
         df = pd.DataFrame(hdul[1].data)
+
+    print(patch, "Loaded data from", inpath)
             
     # Drop undesired columns 
     df.drop(['THELI_NAME','2D_measurement_variance', '2D_measurement_variance_corr'], 
             axis=1, inplace=True)
     
-    print("Succeed in construction for", inpath)
+    print(patch, "Undesired columns dropped.")
 
     # Total objects
     Ntot = len(df)
@@ -94,34 +111,103 @@ def SelecFunc(patch, inpath, outpath, pq):
     # Selection
     df = df[df.GAAP_Flag_ugriZYJHKs == 0]
     
-    print("Succeed in selection", inpath)
+    print(patch, "Succeed in selection.")
 
     # objects after selection
     Ns = len(df)
 
-    hdf.put(key="whole", value=df, format='table', append=True, data_columns=True)
-    hdf.close()
+    # save data
+    outpath = outdir + patch + ".feather"
+    df = df.reset_index(drop=True)
+    df.to_feather(outpath)
+    print(patch, "Data saved to", outpath)
 
     # data information
-    logdata = {"patch": patch, 'Ntot': Ntot, 'Ns': Ns}
+    logdata = {"patch": patch, 'totalNumber': Ntot, 'selectedNumber': Ns}
     pq.put(logdata)
 
-    print("Data saved in", outpath)
+    print(patch, "Finished selection of targets (SelecFunc).")
+
+
+def BinFunc(patch, inpath, outdir, Z, bins, pq):
+    """
+    Function for redshift binning
+    """
+    print(patch, "Start redshift binning (BinFunc)...")
+
+    data = feather.read_dataframe(inpath)
+    print(patch, "Loaded data from", inpath)
+
+    for i in range(len(bins)-1):
+    
+        zmin = bins[i] * 1e-1
+        zmax = bins[i+1] * 1e-1
+        dat = data[(data[Z] > zmin) & (data[Z] <= zmax)]
+        print(patch, "Succeed in binning", str(bins[i]) + str(bins[i+1]))
+
+        outpath = outdir + patch + '__' + Z + '__' + str(bins[i]) + str(bins[i+1]) + '.feather'
+        dat = dat.reset_index(drop=True)
+        dat.to_feather(outpath)
+        print(patch, "Data saved to", outpath)
+
+        # selected numbers
+        N_bin = len(dat)
+
+        # data information
+        save_key = Z + '__' + str(bins[i]) + str(bins[i+1])
+        logdata = {"patch": patch, 'bin': save_key, 'number': N_bin}
+        pq.put(logdata)
+
+    print(patch, "Finished redshift binning (BinFunc).")
+
+
+def BinCountFunc(infpath, outpath, Z, bins):
+    """
+    Function for number counting for binning results
+    """
+    print("Start number counting (BinCountFunc)...")
+
+    data = pd.read_csv(infpath)
+
+    # data information
+    log = open(outpath, "w")
+    print("bin,number", file=log)
+
+    res = []
+    for i in range(len(bins)-1):
+        key = Z + '__' + str(bins[i]) + str(bins[i+1])
+        
+        N = np.sum(data[data.bin==key]['number'].values)
+        print("Number in bin (", str(bins[i] * 1e-1), ",", str(bins[i+1] * 1e-1), "]:", N)
+        print(key, N, sep=',', file=log)
+
+        res.append(N)
+
+    N_tot = np.sum(res)
+    print("Total Number in all bins:", N_tot)
+    print('total', N_tot, sep=',', file=log)
+    log.close()
+
+    print("Finished number counting (BinCountFunc)...")
+
+
 
 if __name__ == "__main__":
+
+    # ++++++++++++++++++++++++++ SelecFunc
+    import time
+    start_SelecFunc = time.time()
 
     # input
     inpathF = "/disks/shear15/ssli/KV450/KV450_"
     inpathP = "_reweight_3x4x4_v2_good.cat"
+    pathGL = ["G9","G12","G15","G23","GS"]
 
     # output
-    outpathF = "/disks/shear15/ssli/KV450/CorrFunc/data/"
-    outpathP = ".h5"
+    outdir = "/disks/shear15/ssli/KV450/selected/"
     
     # data information
-    log = open("/disks/shear15/ssli/KV450/CorrFunc/log/log_data.csv", "w")
-
-    pathGL = ["G9","G12","G15","G23","GS"]
+    log_data = open("/disks/shear15/ssli/KV450/selected/log/log_data.csv", "w")
 
     # for mp
     jobs = []
@@ -129,22 +215,89 @@ if __name__ == "__main__":
 
     for s in pathGL:
         inpath = inpathF + s + inpathP
-        outpath = outpathF + s + outpathP
 
-        p = mp.Process(target=SelecFunc, args=(s, inpath, outpath, pq))
+        p = mp.Process(target=SelecFunc, args=(s, inpath, outdir, pq))
         jobs.append(p)
         p.start()
 
     for p in jobs:
         p.join()
 
-    print("All processing done.")
     print("Start saving data information.")
 
     # data information
-    print("patch,totalNumber,selectedNumber", file=log)
+    print("patch,totalNumber,selectedNumber", file=log_data)
     while not pq.empty():
         tmp = pq.get()
-        print(tmp["patch"], tmp['Ntot'], tmp['Ns'], sep=',', file=log)
+        print(tmp["patch"], tmp['totalNumber'], tmp['selectedNumber'], sep=',', file=log_data)
 
-    print("All done.")
+    log_data.close()
+    print("Done for SelecFunc.")
+    end_SelecFunc = time.time()
+
+
+    # ++++++++++++++++++++++++++ BinFunc
+    start_BinFunc = time.time()
+
+    bins = [1, 3, 5, 7, 9, 12]
+    patches = ["G9","G12","G15","G23","GS"]
+    # column used as redshift
+    Z = 'Z_B'
+
+    # inpath directory
+    inpathF = "/disks/shear15/ssli/KV450/selected/"
+
+    # outpath
+    outdir = "/disks/shear15/ssli/KV450/selected/"
+
+    ### running information
+    log_bins = open("/disks/shear15/ssli/KV450/selected/log/log_bins.csv", "w")
+
+    # for mp
+    jobs = []
+    pq = mp.Queue()
+
+    for patch in patches:
+        inpath = inpathF + patch + ".feather"
+
+        p = mp.Process(target=BinFunc, args=(patch, inpath, outdir, Z, bins, pq))
+        jobs.append(p)
+        p.start()
+
+    for p in jobs:
+        p.join()
+
+    print("Start saving data information.")
+
+    # data information
+    print("patch,bin,number", file=log_bins)
+    while not pq.empty():
+        tmp = pq.get()
+        print(tmp["patch"], tmp['bin'], tmp['number'], sep=',', file=log_bins)
+
+    log_bins.close()
+    print("Done for BinFunc.")
+
+    end_BinFunc = time.time()
+
+    # ++++++++++++++++++++++++++ BinCountFunc
+    start_BinCountFunc = time.time()
+
+    bins = [1, 3, 5, 7, 9, 12]
+    # column used as redshift
+    Z = 'Z_B'
+
+    # path directory
+    infpath = "/disks/shear15/ssli/KV450/selected/log/log_bins.csv"
+
+    # outpath
+    outpath = "/disks/shear15/ssli/KV450/selected/log/log_BinCount.csv"
+
+    BinCountFunc(infpath, outpath, Z, bins)
+
+    end_BinCountFunc = time.time()
+
+    # +++++++++++++++++++++++++ Running time
+    print("Running time for SelecFunc", end_SelecFunc-start_SelecFunc, "seconds.")
+    print("Running time for BinFunc", end_BinFunc-start_BinFunc, "seconds.")
+    print("Running time for BinCountFunc", end_BinCountFunc-start_BinCountFunc, "seconds.")
