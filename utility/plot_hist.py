3d6b1b5464beef31d0ebca6fe079c10414832295
Shun-Sheng Li <41175095+lshuns@users.noreply.github.com>
2019-11-21 17:14:03 +0100

    create utility with plot_hist.py

---
+++ utility/plot_hist.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 15 13:50:48 2019
+
+@author: ssli
+
+make the histogram plot
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+mpl.use('Agg')
+mpl.rcParams['xtick.direction'] = 'in'
+mpl.rcParams['ytick.direction'] = 'in'
+mpl.rcParams['xtick.top'] = True
+mpl.rcParams['ytick.right'] = True
+
+
+def HistPlotFunc(values, wgs, outpaths, 
+    DENSITY, HISTTYPE, NBs, COLORs, LABLEs,
+    XLABEL, YLABEL):
+    """
+    Function for histogram plot
+    """
+    print("Make the histogram plot (HistPlotFunc)...")
+
+    fig = plt.figure()
+    for i in range(len(values)):
+        value = values[i]
+        wg = wgs[i]
+
+        NB = NBs[i]
+        COLOR = COLORs[i]
+        LABLE = LABLEs[i]
+    
+        plt.hist(x=value, bins=NB, density=DENSITY, weights=wg, color=COLOR, label=LABLE, histtype=HISTTYPE)
+
+    plt.legend(frameon=False)
+    plt.xlabel(XLABEL)
+    plt.ylabel(YLABEL)
+    for path in outpaths:
+        plt.savefig(path)
+        print("Hist plot saved to", path)
+    plt.close()
+
+    print("Finished the histogram plot (HistPlotFunc).")
