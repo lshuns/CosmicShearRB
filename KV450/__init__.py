844acbed4754a3d67c7d10d76ac2e0d19cf986b8
Shun-Sheng Li <41175095+lshuns@users.noreply.github.com>
2020-01-30 15:11:53 +0100

    KV450/ entirely re-organized

---
+++ KV450/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 13 15:25:29 2020
+
+@author: ssli
+
+SelecFunc (mp supported):
+    set selection and re-save the original data
+
+BinFunc (mp supported):
+    binning data and save
+BinCountFunc
+    Bin number counts
+        associated with BinFunc with pq!=None
+
+PatchFunc:
+    combining all patches
+"""
+
+from KV450.DataPreprocess import SelecFunc, BinFunc, BinCountFunc, PatchFunc
