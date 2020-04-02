82d7d154a820a4ecf251e2524601d27d23f65c22
Shun-Sheng Li <1006682997@qq.com>
2020-04-02 22:03:51 +0200

    2020-04-02 running

--- scripts/RunChi2Test.py
+++ scripts/RunChi2Test.py
@@ -35,6 +35,7 @@ paths['param'] =  '/net/raam/data1/surfdrive_ssli/Projects/6CosmicShear_RB/Cosmi
 
 
 # name_param_file = 'kv450_cf_best.param'
+# name_param_file = 'kv450_cf_null_nuisance.param'
 # name_param_file = 'kv450_cf_mean.param'
 name_param_file = 'kv450_cf_Planck.param'
 
@@ -61,8 +62,8 @@ nzbins = len(data_whole.const['z_bins_min'])
 nzcorrs = int(nzbins * (nzbins + 1) / 2)
 #
 chi2_whole, dof_whole = Chi2Test.Chi2SingleFunc(nzbins, nzcorrs, theta_bins_whole, mask_whole, data_whole, xi_obs_whole, xi_theo_whole)
-print("chi2_whole", chi2_whole)
-print("dof_whole", dof_whole)
+# print("chi2_whole", chi2_whole)
+# print("dof_whole", dof_whole)
 
 # ++++++++++++++++++++++++++++++++++++++++++ red
 # name of parameter/configure files
@@ -88,8 +89,8 @@ nzbins = len(data_red.const['z_bins_min'])
 nzcorrs = int(nzbins * (nzbins + 1) / 2)
 #
 chi2_red, dof_red = Chi2Test.Chi2SingleFunc(nzbins, nzcorrs, theta_bins_red, mask_red, data_red, xi_obs_red, xi_theo_red)
-print("chi2_red", chi2_red)
-print("dof_red", dof_red)
+# print("chi2_red", chi2_red)
+# print("dof_red", dof_red)
 
 # ++++++++++++++++++++++++++++++++++++++++++ blue
 # name of parameter/configure files
@@ -115,12 +116,13 @@ nzbins = len(data_blue.const['z_bins_min'])
 nzcorrs = int(nzbins * (nzbins + 1) / 2)
 #
 chi2_blue, dof_blue = Chi2Test.Chi2SingleFunc(nzbins, nzcorrs, theta_bins_blue, mask_blue, data_blue, xi_obs_blue, xi_theo_blue)
-print("chi2_blue", chi2_blue)
-print("dof_blue", dof_blue)
+# print("chi2_blue", chi2_blue)
+# print("dof_blue", dof_blue)
 
 
-# ++++++++++++++++++++++++++++++++++++++++++++ red - blue
+# ++++++++++++++++++++++++++++++++++++++++++++ blue - red
 inDir_cov12 = '/disks/shear15/ssli/CosmicShear/covariance'
+# file_name_cov12 = 'thps_cov_mar11_list_br.dat'
 file_name_cov12 = 'thps_cov_mar11_usable_br.dat'
 ntheta = 9
 # check the mask is the same
@@ -129,69 +131,75 @@ if np.array_equal(mask_red, mask_blue):
                             data_blue, xi_obs_blue, xi_theo_blue,
                             data_red, xi_obs_red, xi_theo_red,
                             inDir_cov12, file_name_cov12)
-    print("chi2_blue_red", chi2_br)
-    print("dof_blue_red", dof_br)
+    # print("chi2_blue_red", chi2_br)
+    # print("dof_blue_red", dof_br)
 else:
     print("Something wrong with the mask!")
 
+
+print("chi2_whole", chi2_whole)
+print("chi2_red", chi2_red)
+print("chi2_blue", chi2_blue)
+print("chi2_blue_red", chi2_br)
 print("All finished in", time.time()-Start)
 # ('All finished in', 25.485830068588257) (covariance matrix in list form)
 
-
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++ old
-################# results from mean values
-# # whole
-# Intitial cosmological parameters passed to CLASS code:
-# {'omega_cdm': 0.118, 'ln10^{10}A_s': 3.158, 'omega_b': 0.022, 'n_s': 1.021, 'h': 0.745, 'non linear': 'hmcode', 'c_min': 2.484, 'Omega_k': 0.0, 'N_eff': 2.0328, 'N_ncdm': 1.0, 'm_ncdm': 0.06, 'T_ncdm': 0.71611, 'output': 'mPk', 'P_k_max_h/Mpc': 170.0, 'sBBN file': '/net/eemmeer/data1/ssli/class_public/bbn/sBBN.dat', 'k_pivot': 0.05, 'z_max_pk': 5.9750000000000005}
-# sigma8 = 0.8905375160205157
-# Omega_m = 0.25340144300336764
-# h = 0.745
-
-# 'chi2_whole', 187.248500267432
-# 'dof_whole', 195
-
-# #red
-#
-# A_IA = 0.981 
-# ('chi2_red', 158.91163818760813)
-# ('dof_red', 195)
-#
-# A_IA = 0
-# chi2_red 159.10040204122924
-# dof_red 195
-
-# whole matrix
-# A_IA = 0.981 
-# ('chi2_red', 776.1795011987977
-# ('dof_red', 195)
-#
-# A_IA = 0
-# chi2_red 775.7116916240524
-# dof_red 195
-
-# # blue
-#
-# A_IA = 0.981
-# ('chi2_blue', 186.727356105233)
-# ('dof_blue', 195)
-#
-# A_IA = 0
-# chi2_blue 200.51255150505304
-# dof_blue 195
-# All finished in 19.25131869316101
-
-# whole matrix
-# A_IA = 0.981
-# ('chi2_blue', 586.8404108038512
-# ('dof_blue', 195)
-#
-# A_IA = 0
-# chi2_blue 610.9348264268585
-# dof_blue 195
-# All finished in 19.25131869316101
+chi2_blue_red 181.72856546228772
 
 
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++ new
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++ 2020-04-02
+# All finished in 24.952291250228882
+# ########### whole (old covariance)
+# +++ best KV450
+# chi2_whole 181.289777435608
+# +++ IA = 0
+# chi2_whole 182.14159048043757
+# +++ null nuisance
+# chi2_whole 183.03711897015557
+# +++ mean KV450
+# chi2_whole 186.6469440889119
+# +++ Planck
+# chi2_whole 197.15775145270865
+
+# ########### red
+# +++ best KV450
+# chi2_red 159.0857498091302
+# +++ IA = 0
+# chi2_red 157.91943258570063
+# +++ null nuisance
+# chi2_red 157.49834365474632
+# +++ mean KV450
+# chi2_red 158.74952682259186
+# +++ Planck
+# chi2_red 163.18518748760374
+
+# ########### blue
+# +++ best KV450
+# chi2_blue 176.69554322681768
+# +++ IA = 0
+# chi2_blue 179.6342164659231
+# +++ null nuisance
+# chi2_blue 181.4358222272628
+# +++ mean KV450
+# chi2_blue 187.62772845975311
+# +++ Planck
+chi2_blue 202.74030188552254
+
+# ########### blue-red
+# +++ best KV450
+# chi2_blue_red 176.6212374720392
+# +++ IA = 0
+# chi2_blue_red 177.24543612145942
+# +++ null nuisance
+# chi2_blue_red 177.47529222732436
+# +++ mean KV450
+# chi2_blue_red 178.08327855721166
+# +++ Planck
+
+
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++ old 2
 ################# results from max-likelihood values
 # Intitial cosmological parameters passed to CLASS code:
 # {'omega_cdm': 0.05748817, 'ln10^{10}A_s': 4.697331, 'omega_b': 0.0219527, 'n_s': 1.128071, 'h': 0.7801301, 'non linear': 'hmcode', 'c_min': 2.188542, 'Omega_k': 0.0, 'N_eff': 2.0328, 'N_ncdm': 1.0, 'm_ncdm': 0.06, 'T_ncdm': 0.71611, 'output': 'mPk', 'P_k_max_h/Mpc': 170.0, 'sBBN file': '/net/eemmeer/data1/ssli/class_public/bbn/sBBN.dat', 'k_pivot': 0.05, 'z_max_pk': 5.9750000000000005}
@@ -250,4 +258,57 @@ print("All finished in", time.time()-Start)
 # chi2_blue_red 176.81964188989852
 # +++++ Planck values
 # chi2_blue_red 179.9253175222906
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++ old
+################# results from mean values
+# # whole
+# Intitial cosmological parameters passed to CLASS code:
+# {'omega_cdm': 0.118, 'ln10^{10}A_s': 3.158, 'omega_b': 0.022, 'n_s': 1.021, 'h': 0.745, 'non linear': 'hmcode', 'c_min': 2.484, 'Omega_k': 0.0, 'N_eff': 2.0328, 'N_ncdm': 1.0, 'm_ncdm': 0.06, 'T_ncdm': 0.71611, 'output': 'mPk', 'P_k_max_h/Mpc': 170.0, 'sBBN file': '/net/eemmeer/data1/ssli/class_public/bbn/sBBN.dat', 'k_pivot': 0.05, 'z_max_pk': 5.9750000000000005}
+# sigma8 = 0.8905375160205157
+# Omega_m = 0.25340144300336764
+# h = 0.745
+
+# 'chi2_whole', 187.248500267432
+# 'dof_whole', 195
+
+# #red
+#
+# A_IA = 0.981 
+# ('chi2_red', 158.91163818760813)
+# ('dof_red', 195)
+#
+# A_IA = 0
+# chi2_red 159.10040204122924
+# dof_red 195
+
+# whole matrix
+# A_IA = 0.981 
+# ('chi2_red', 776.1795011987977
+# ('dof_red', 195)
+#
+# A_IA = 0
+# chi2_red 775.7116916240524
+# dof_red 195
+
+# # blue
+#
+# A_IA = 0.981
+# ('chi2_blue', 186.727356105233)
+# ('dof_blue', 195)
+#
+# A_IA = 0
+# chi2_blue 200.51255150505304
+# dof_blue 195
+# All finished in 19.25131869316101
+
+# whole matrix
+# A_IA = 0.981
+# ('chi2_blue', 586.8404108038512
+# ('dof_blue', 195)
+#
+# A_IA = 0
+# chi2_blue 610.9348264268585
+# dof_blue 195
+# All finished in 19.25131869316101
 
